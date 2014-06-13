01f8212b06db4eff341b2c8239dc49b392ac0f68
Demetrio <10_08_1995@mail.ru>
2014-06-13 22:04:12 +0600

    Создание БД

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,7 +2,6 @@
 #include "ui_mainwindow.h"
 
 #include <QMessageBox>
-//#include <iostream>
 #include <QDebug>
 #include <QDir>
 #include <QtSql>
@@ -13,21 +12,61 @@ bool connectDB(){
     const QString dbPass("");
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-/*    QDir objQdir;
-    objQdir.setPath( QDir::homePath() );
-    objQdir.
-    objQdir.mkdir( "LibertaSoft" );
-    objQdir.mkdir( "LibertaSoft/DatabaseAnime/" );*/
-    db.setDatabaseName( QDir::homePath() +"/DatabaseAnime.sqlite");
+    QDir objQdir;
+    QString DBPath( QDir::homePath() + "/."+QApplication::organizationName()+"/"+QApplication::applicationName() );
+    bool isOk = objQdir.mkpath( DBPath );
+    if( !isOk ){
+        qDebug() << "Cannot createed app directory in home path";
+        QMessageBox::warning(0, QObject::tr("Внимание"), QObject::tr("Не удалось создать директорию для базы данных.") );
+        return false;
+    }
+
+    db.setDatabaseName( DBPath +"/DatabaseAnime.sqlite");
     db.setUserName( dbUser );
     db.setHostName( dbHost );
     db.setPassword( dbPass );
     if( !db.open() ){
-        qDebug() << "Cannot Open DataBase: " << db.lastError().text();
-        QMessageBox::information(0,"Cannot Open DataBase", db.lastError().text());
+        qDebug() << "Cannot open database: " << db.lastError().text();
+        QMessageBox::warning(0, QObject::tr("Внимание"), QObject::tr("Не удалось подключится к БД.") );
         return false;
     }
+    return true;
+}
 
+bool createTable_AnimeSerials(){
+    QString str = "CREATE TABLE IF NOT EXISTS animeSerials( "
+                  "id INTEGER PRIMARY KEY NOT NULL,"
+                  "isHaveLooked   INTEGER, "
+                  "isEditingDone  INTEGER, "
+                  "Title          VARCHAR(128), "
+                  "OrigTitle      VARCHAR(128), "
+                  "Director       VARCHAR(32), "
+                  "PostScoring    VARCHAR(128), "
+                  "SeriesTV       INTEGER, "
+                  "SeriesOVA      INTEGER, "
+                  "SeriesONA      INTEGER, "
+                  "SeriesSpecial  INTEGER, "
+                  "SeriesFilm     INTEGER, "
+                  "vSeriesTV      INTEGER, "
+                  "vSeriesOVA     INTEGER, "
+                  "vSeriesONA     INTEGER, "
+                  "vSeriesSpecial INTEGER, "
+                  "vSeriesFilm    INTEGER, "
+                  "Year           INTEGER, "
+                  "Season         INTEGER, "
+                  "Studios        INTEGER, "
+                  "Tags           VARCHAR(256), "
+                  "Description    TEXT, "
+                  "URL            VARCHAR(256), "
+                  "Dir            VARCHAR(256), "
+                  "ImagePath      VARCHAR(256) "
+                  ");";
+    QSqlQuery query;
+    if( !query.exec(str) ){
+        qDebug() << "Table AnimeSerials is not created! Error: " << query.lastError();
+        QMessageBox::warning(0, QObject::tr("Внимание"), QObject::tr("Извините. Произошла ошибка при создании таблицы в БД.") );
+        return false;
+    }
     return true;
 }
 
@@ -38,7 +77,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     ui->lbl_AppTitle->setText( QApplication::applicationDisplayName() );
     ui->Lbl_VVersion->setText( QApplication::applicationVersion() );
-    //ui->Lbl_VVersion->setText( QApplication::platformName() );
 
     QSettings settings;
     bool set_enableBtnAnime     = settings.value("enableElem/BtnSwitchSection/Anime",     true).toBool();
@@ -47,7 +85,6 @@ MainWindow::MainWindow(QWidget *parent) :
     bool set_enableBtnDorama    = settings.value("enableElem/BtnSwitchSection/Dorama",   false).toBool();
     bool set_enableBtnEditable  = settings.value("enableElem/BtnSwitchSection/Editable",  true).toBool();
     bool set_enableBtnLookLater = settings.value("enableElem/BtnSwitchSection/LookLater", true).toBool();
-    bool set_isFullScreen       = settings.value("MainWindow/isFullScreen",              false).toBool();
 
     if( set_enableBtnAnime ){
         btnAnime = new QPushButton( tr("Аниме"), this );
@@ -77,7 +114,8 @@ MainWindow::MainWindow(QWidget *parent) :
     this->restoreGeometry( settings.value("MainWindow/Geometry").toByteArray() );
     this->restoreState( settings.value("MainWindow/State").toByteArray() );
 
-//    connectDB();
+    connectDB();
+    createTable_AnimeSerials();
 }
 
 void MainWindow::closeEvent(QCloseEvent* ){
@@ -100,38 +138,5 @@ void MainWindow::on_PButton_Options_clicked()
 
 void MainWindow::on_PBtn_Create_clicked()
 {
-    QMessageBox::information(0,"Click","Create DB Begin");
 
-    /* */
-    QString str = "CREATE TABLE animeSerials( "
-                  "id INTEGER PRIMARY KEY NOT NULL,"
-                  "isHaveLooked   INTEGER, "
-                  "isEditingDone  INTEGER, "
-                  "Title          VARCHAR(128), "
-                  "OrigTitle      VARCHAR(128), "
-                  "Director       VARCHAR(48), "
-                  "PostScoring    VARCHAR(64), "
-                  "SeriesTV       INTEGER, "
-                  "SeriesOVA      INTEGER, "
-                  "SeriesONA      INTEGER, "
-                  "SeriesSpecial  INTEGER, "
-                  "SeriesFilm     INTEGER, "
-                  "vSeriesTV      INTEGER, "
-                  "vSeriesOVA     INTEGER, "
-                  "vSeriesONA     INTEGER, "
-                  "vSeriesSpecial INTEGER, "
-                  "vSeriesFilm    INTEGER, "
-                  "Year           INTEGER, "
-                  "Season         INTEGER, "
-                  "Studios        INTEGER, "
-                  "Tags           VARCHAR(256), "
-                  "Description    TEXT, "
-                  "URL            VARCHAR(256), "
-                  "Dir            VARCHAR(256), "
-                  "ImageParh      VARCHAR(256) "
-                  ");";
-    QSqlQuery query;
-    if( !query.exec(str) ){
-        qDebug() << "DB is not created! Error: " << query.lastError();
-    }
 }
