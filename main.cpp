8f9b9b3e01aeeea90f4146aa9f60f41d9c8899fa
Demetri0 <d3i0@ya.ru>
2014-12-01 21:21:59 +0600

    dark style

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <QStyleFactory>
+
 #include <QDebug>
 
 void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
@@ -34,6 +36,28 @@ int main(int argc, char *argv[])
     #ifndef QT_DEBUG
         qInstallMessageHandler(myMessageOutput);
     #endif
+
+        qApp->setStyle(QStyleFactory::create("Fusion"));
+
+        QPalette darkPalette;
+        darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+        darkPalette.setColor(QPalette::WindowText, Qt::white);
+        darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+        darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+        darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+        darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+        darkPalette.setColor(QPalette::Text, Qt::white);
+        darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+        darkPalette.setColor(QPalette::ButtonText, Qt::white);
+        darkPalette.setColor(QPalette::BrightText, Qt::red);
+        darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+        darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+        darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+        qApp->setPalette(darkPalette);
+
+        qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
 
 
     QApplication app(argc, argv);
