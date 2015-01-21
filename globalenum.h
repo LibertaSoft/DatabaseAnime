036282555acc7bedfd59f70db6631ecef46f5ce9
Demetri0 <d3i0@ya.ru>
2015-01-21 20:38:00 +0600

    c++11 enumeration style, dont work

--- globalenum.h
+++ globalenum.h
@@ -1,9 +1,15 @@
 #ifndef GLOBALENUM_H
 #define GLOBALENUM_H
 
+/*#include <QMetaType>*/
+
 namespace sections {
     enum section{none = 0, anime, manga, amv, dorama};
 }
+/* #Note : Problems with QVariant */
+/*enum class Section : int {None, Anime, Manga, Amv, Dorama};*/
+/*Q_DECLARE_METATYPE(Section)*/
+
 namespace Filter {
     enum filter{all = 0, editing, wanttolook, tv, ova, ona, special, movie, looked};
 }
