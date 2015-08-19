f821fe646cf2783102b6bf2ba95b07235c8c9c27
Demetri0 <d3i0@ya.ru>
2015-08-19 19:03:34 +0600

    [1.4.1] Release

--- DatabaseAnime/main.cpp
+++ DatabaseAnime/main.cpp
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
     app.setOrganizationName("LibertaSoft");
     app.setOrganizationDomain("https://github.com/LibertaSoft");
     app.setApplicationName("DatabaseAnime");
-    app.setApplicationVersion("1.4.0");
+    app.setApplicationVersion("1.4.1");
     app.setApplicationDisplayName( QObject::tr("Database Anime") );
     app.setWindowIcon( QIcon("://images/DBA_Icon.png") );
 
