1264d7d9863f842d217be989776464cd8b233940
Demetri0 <d3i0@ya.ru>
2016-11-29 00:41:26 +0600

    upd

---
+++ DatabaseAnime/widgets/searchitemdelegate.cpp
@@ -0,0 +1,6 @@
+#include "searchitemdelegate.h"
+
+SearchItemDelegate::SearchItemDelegate()
+{
+
+}
---
+++ DatabaseAnime/widgets/searchitemdelegate.h
@@ -0,0 +1,11 @@
+#ifndef SEARCHITEMDELEGATE_H
+#define SEARCHITEMDELEGATE_H
+
+
+class SearchItemDelegate
+{
+public:
+    SearchItemDelegate();
+};
+
+#endif // SEARCHITEMDELEGATE_H
